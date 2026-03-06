46e34263d5af26678e4f9c51e46b47c59caeb128
Ruben Leis <rubenleisfp@gmail.com>
2026-03-06 13:40:26 +0100

    Ejemplo estricto

--- src/main/java/com/fp/artista_hexagonal/artist/domain/port/in/ArtistaUseCase.java
+++ src/main/java/com/fp/artista_hexagonal/artist/domain/port/in/ArtistaUseCase.java
@@ -5,6 +5,6 @@ import com.fp.artista_hexagonal.artist.domain.model.Artista;
 import java.util.List;
 
 public interface ArtistaUseCase{
-    public Artista crearArtista(String nombre);
-    public List<Artista> obtenerTodos();
+    Artista crearArtista(String nombre);
+    List<Artista> obtenerTodos();
 }
